770a009bfd4c987e8b17416a246e760935dc5bd9
faruq <faruqmansuri707@gmail.com>
2025-07-09 22:49:07 +0530

    add upload image to supabase functionality

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -2,19 +2,24 @@ import Buttons from '@/components/form/Buttons';
 import FormContainer from '@/components/form/FormContainer';
 import FormInput from '@/components/form/FormInput';
 import { createProfileAction } from '@/utils/action';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const CreateProfilePage = () => {
+const CreateProfilePage = async () => {
+  const user = await currentUser();
+  if (user?.privateMetadata?.hasProfile) redirect('/');
+
   return (
     <section>
-      <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>New User</h1>
       <div className='border p-8 rounded-md '>
         <FormContainer action={createProfileAction}>
           <div className='grid gap-4 md:grid-cols-2 mt-4'>
             <FormInput name='firstName' type='text' label='first name' />
             <FormInput name='lastName' type='text' label='last name' />
-            <FormInput name='userName' type='text' label='Username' />
+            <FormInput name='username' type='text' label='Username' />
           </div>
-          <Buttons text='Create Profile' className='mt-8' />
+          <Buttons text={'Create Profile'} className='mt-8' />
         </FormContainer>
       </div>
     </section>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import Buttons from '@/components/form/Buttons';
+import FormContainer from '@/components/form/FormContainer';
+import FormInput from '@/components/form/FormInput';
+import ImageInputContainer from '@/components/form/ImageInputContainer';
+import {
+  updateProfileAction,
+  fetchProfile,
+  updateProfileImageAction,
+} from '@/utils/action';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+const UpdateProfile = async () => {
+  const profile = await fetchProfile();
+
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>User Profile</h1>
+      <div className='border p-8 rounded-md '>
+        <ImageInputContainer
+          action={updateProfileImageAction}
+          image={profile.profileImage}
+          name={profile.username}
+          text='Update Profile Image'
+        />
+        <FormContainer action={updateProfileAction}>
+          <div className='grid gap-4 md:grid-cols-2 mt-4'>
+            <FormInput
+              name='firstName'
+              type='text'
+              label='first name'
+              defaultValue={profile.firstName}
+            />
+            <FormInput
+              name='lastName'
+              type='text'
+              label='last name'
+              defaultValue={profile.lastName}
+            />
+            <FormInput
+              name='username'
+              type='text'
+              label='Username'
+              defaultValue={profile.username}
+            />
+          </div>
+          <Buttons text={'Update Profile'} className='mt-8' />
+        </FormContainer>
+      </div>
+    </section>
+  );
 };
 
-export default ProfilePage;
+export default UpdateProfile;
