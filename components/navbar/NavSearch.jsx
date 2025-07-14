dc24c4020059efd85d42140753c7d67337994f97
faruq <faruqmansuri707@gmail.com>
2025-07-14 23:27:27 +0530

    redirect user to home page when user tries to perform search from other pages.

--- components/navbar/NavSearch.jsx
+++ components/navbar/NavSearch.jsx
@@ -1,12 +1,11 @@
 'use client';
 import { Input } from '../ui/input';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { useState, useEffect } from 'react';
 
 const NavSearch = () => {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
 
   const [searchTerm, setSearchTerm] = useState(
